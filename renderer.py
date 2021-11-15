f41b6330d2a5b2f7c0ff08205d97cef53974283f
Keeran Marquis <k.marquis@kd-tech.co.uk>
2021-11-15 17:49:26 +0000

    Reacting of code and templates
    This PR has the following updates:
    - Updated the Junos templates to `set` format and moves some of the `curly` format into comments
    - Black'd the renderer.py to standardise the python code
    - Updated the requirement.txt as the modules were very much out of date and not compatiable with some python3 things

--- renderer.py
+++ renderer.py
@@ -10,9 +10,11 @@ import netaddr
 from jinja2 import Environment, FileSystemLoader
 from jinja2.exceptions import *
 
+
 def ipad(network):
     net = ipaddress.ip_network(network)
     return net
+
 
 def ipaddr(network, attr=None):
     netw = netaddr.IPNetwork(network)
@@ -21,31 +23,53 @@ def ipaddr(network, attr=None):
     else:
         return netw
 
+
 def md5(password):
     md5 = md5_crypt.hash(password)
     return md5
 
+
 def sha(password):
-	pwd = pbkdf2_sha1.hash(password)
-	return pwd
+    pwd = pbkdf2_sha1.hash(password)
+    return pwd
+
+
+def debugging():
+    err = "This an error is here"
+    return err
+
 
 def main():
     try:
-        data = yaml.load(open(sys.argv[1]))
+        data = yaml.safe_load(open(sys.argv[1]))
         template_dir = FileSystemLoader(os.path.dirname(os.path.realpath(__file__)))
-        env = Environment(loader=template_dir, trim_blocks=True, lstrip_blocks=True,
-    	     keep_trailing_newline=True, newline_sequence='\n')
+        env = Environment(
+            loader=template_dir,
+            trim_blocks=True,
+            lstrip_blocks=True,
+            keep_trailing_newline=True,
+            newline_sequence='\n',
+        )
         env.filters['ipaddr'] = ipaddr
         env.filters['sha'] = sha
         env.filters['md5'] = md5
         env.filters['ipad'] = ipad
+        env.filters['debugging'] = debugging
         tpl = env.get_template(sys.argv[2])
         print(tpl.render(data))
     except (TemplateSyntaxError, UndefinedError) as e:
         print('An Error has occured: {0}'.format(e))
 
+
 if __name__ == '__main__':
-     if len(sys.argv) < 3:
-         print(info(lightred('Please provide the following: "python3 {0} yaml_file j2_template"'.format(sys.argv[0]))))
-         sys.exit(1)
-     main()
+    if len(sys.argv) < 3:
+        print(
+            info(
+                lightred(
+                    'Please provide the following: "python3 {0} yaml_file j2_template"'
+                    .format(sys.argv[0])
+                )
+            )
+        )
+        sys.exit(1)
+    main()
